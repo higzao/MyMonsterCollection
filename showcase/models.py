638a2aa2732977a419f0371ebe7cebe5125442de
Higzao <higor.sis13@outlook.com>
2024-10-30 20:57:47 -0300

    MMC New

--- showcase/models.py
+++ showcase/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Can(models.Model):
+    name = models.CharField(max_length=100)
+    line = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    continent = models.CharField(max_length=50)
+    size = models.IntegerField()  # Armazena o valor numérico do tamanho em ml
+    sku = models.CharField(max_length=20, blank=True, null=True)
+    manufacturer = models.CharField(max_length=50)
+
+    # Colors
+    main_color = models.CharField(max_length=20)
+    detail_color = models.CharField(max_length=20)
+    color_lid = models.CharField(max_length=20)
+    color_top = models.CharField(max_length=20)
+
+    logo_lid = models.BooleanField(default=False)
+    sugar_warning = models.BooleanField(default=False)
+    condition = models.IntegerField()  # Assume um valor de 0 a 100 para condição
+    top_intact = models.BooleanField(default=True)
+    
+    # Promotion
+    is_promotion = models.BooleanField(default=False)
+    promotion_name = models.CharField(max_length=100, blank=True, null=True)
+    promotion_details = models.TextField(blank=True, null=True)
+
+    bottle = models.BooleanField(default=False)
+    tasted = models.BooleanField(default=False)
+
+    # Notes
+    top_notes = models.TextField(blank=True, null=True)
+    bottom_notes = models.TextField(blank=True, null=True)
+
+    # Images
+    img_front = models.CharField(max_length=100)
+    img_back = models.CharField(max_length=100)
+    img_back_2 = models.CharField(max_length=100)
+    img_top = models.CharField(max_length=100)
+
+    # Gift information
+    is_gift = models.BooleanField(default=False)
+    friend_name = models.CharField(max_length=100, blank=True, null=True)
+    gift_date = models.DateField(blank=True, null=True)
+
+def __str__(self):
+    return self.name
--- showcase/views.py
+++ showcase/views.py
@@ -1,7 +1,79 @@
 from django.shortcuts import render
 
+
 def index (request):
-    return render(request, 'showcase/index.html')
+    data = {
+    1: {'name': 'Monster Energy Dragon Ice Tea Lemon',
+        'line': 'Dragon Ice Tea',
+        'country': 'Brazil',
+        'continent' : 'South America',
+        'size': '473ml',
+        'sku': '0819B',
+        'manufacturer': 'ball',
+        'color': {
+            'main': 'green',
+            'details': 'green',
+            'lid': 'green',
+            'top': 'silver'},
+        'logo_lid': True,
+        'sugar_warning': False,
+        'condition': 97,
+        'top_intact': False,
+        'promotion': {
+            'is_promotion': False,
+            'name': '',
+            'details': ''},
+        'bottle': False,
+        'tasted': True,
+        'notes': {
+            'top': 'CHÁ COM LIMÃO / LEMON / NÃO GASEFICADO',
+            'bottom': 'COMPOSTO LÍQUIDO PRONTO PARA O CONSUMO À BASE DE TAURINA E CAFEÍNA COM SUCO DE FRUTAS E CHÁ. CONTÉM AROMATIZANTE.'},
+        'images': {
+            'front': 'br-dragonicetea-front.png',
+            'back': 'xxxx',
+            'back_2': 'xxxx',
+            'top': 'xxx'},
+        'gift': {
+            'is_gift': True,
+            'friend': 'Ludwig'}
+     },
+    2: {'name': 'Monster Energy Original (COD)',
+        'line': 'Energy',
+        'country': 'Poland',
+        'continent' : 'Europe',
+        'size': '500ml',
+        'sku': '0623',
+        'manufacturer': 'CP',
+        'color': {
+            'main': 'black',
+            'details': 'green',
+            'lid': 'black',
+            'top': 'silver'},
+        'logo_lid': False,
+        'sugar_warning': False,
+        'condition': 100,
+        'top_intact': True,
+        'promotion': {
+            'is_promotion': True,
+            'name': 'Call of Duty',
+            'details': 'não sei qual foi kkk'},
+        'bottle': False,
+        'tasted': True,
+        'notes': {
+            'top': 'L-KARNITYNA + TAURYNA + ZENSZEN + WITAMINY Z GRUPY B',
+            'bottom': ''},
+        'images': {
+            'front': 'pl-og-cod-front.png',
+            'back': 'xxxx',
+            'back_2': 'xxxx',
+            'top': 'xxx'},
+        'gift': {
+            'is_gift': False,
+            'friend': 'Ludwig'}
+
+    }
+}
+    return render(request, 'showcase/index.html', {'cards':data})
 
 def image (request):
     return render(request, 'showcase/image.html')
