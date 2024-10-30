b31e2ef3dcc3ae0d20849ea6a2e18dd68b5d0d5e
Higzao <higor.sis13@outlook.com>
2024-10-30 17:07:48 -0300

    Change project to My Monser Collection

--- showcase/urls.py
+++ showcase/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from showcase.views import index, image
 
 urlpatterns = [
-    path('', index),
-    path('image/', image)
+    path('', index, name = 'index'),
+    path('image/', image, name = 'image')
 ]
